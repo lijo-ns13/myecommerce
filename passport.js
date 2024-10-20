7ae82cb6afb3d863add6d4a5e55d2e3e59a3050f
lijo <lijons13@gmail.com>
2024-10-20 18:53:47 +0530

    minor changes in passport.js

--- passport.js
+++ passport.js
@@ -15,7 +15,7 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: 'http://lijons.shop/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const usersCollection = db.collection('users');
